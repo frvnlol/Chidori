5d358487e5a0cc3927e3b461d33e3bca0ec7b6a5
frvnci5 <frvncis@outlook.com>
2023-10-02 21:32:02 -0700

    appjs data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Page from './components/Page';
+import React, { useState } from 'react';
 
 function App() {
+  const [pages] = useState([
+    {
+      name: "about me"
+    },
+    {
+      name: "portfolio"
+    },
+    {
+      name: "contact"
+    },
+    {
+      name: "resume"
+    },
+  ])
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
     <div>
-      <Header />
+      <Header pages= { pages } setCurrentPage= { setCurrentPage } currentPage= { currentPage } />
       <main>
         <p> something in it </p>
       </main>
