b7cb41a715099a826ce5a8036343a780be13633d
frvnci5 <frvncis@outlook.com>
2023-10-02 22:00:04 -0700

    index nav bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function Header(props) {
-
+  const { pages = [], setCurrentPage, currentPage } = props;
   return (
     <header className="flex-row space-between px-1">
+      <nav>
+        <ul>{pages.map((Page) => (
+            <li key= {Page.name} onClick= {() => setCurrentPage(Page)}>{Page.name}</li>
+        ))}</ul>
+      </nav>
       <h1>Hi I'm Francis</h1>
     </header>
   );
-
 }
 
 export default Header;
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import About from '../About';
+import Contact from '../Contact';
+import Portfolio from '../Portfolio';
+import Resume from '../Resume';
 
 function Page(props) {
-
+ const { currentPage } = props;
+ const { name } = currentPage;
+ const render = () => {
+    switch(name) {
+        case 'about me': 
+            return <About />;
+        case 'portfolio':
+            return <Portfolio />;
+        case 'resume':
+            return <Resume />;
+        case 'contact':
+            return <Contact />;
+        
+        default: 
+        return <About />;
+    }
+ }
   return (
-    <section>
-      <h2>Kick rocks</h2>
+      <section>
+          <h2>{ name }</h2>
+          { render() }
     </section>
   );
 
